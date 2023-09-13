7758297a3313ce1f129f97dd686d1b2af63bb018
LuanCKRG <crackersage@gmail.com>
2023-09-13 18:47:46 -0400

    v56

--- src/routes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var scrape = require('./controllers/scrape');
-
-const e=async t=>{t.get("/",scrape.safra_data.index),t.post("/",scrape.safra_data.get);};
-
-exports.routes = e;
--- src/server.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var app = require('./app');
-
-const s=3e3;app.app.listen({port:s,host:"0.0.0.0"},(t,i)=>{t&&(app.app.log.error(t),process.exit(1)),app.app.log.info(`Fastify is listening on port: ${i}`);});
