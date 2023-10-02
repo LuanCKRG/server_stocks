1418fd3a060d53e95467f43080937c4febc8a76e
LuanCKRG <crackersage@gmail.com>
2023-10-01 21:11:47 -0400

    v:last-2

--- src/lib/queue.ts
+++ src/lib/queue.ts
@@ -9,43 +9,43 @@ import puppeteer from "puppeteer"
 
 export const queue = {
   add: (stock: string) => {
-    const StockQueue = new Queue(stock, queueConfig)
+    const StockQueue = new Queue('stock', queueConfig)
 
-    StockQueue.add(stock, stock)
-
-    const StockWorker = new Worker(stock,
-      async (job) => {
-        const {data: token} = job
-
-        console.log(token)
-
-        const browser = await puppeteer.launch(puppeteerConfig)
-        const page = await browser.newPage()
-        
-        await get_data_xp(token)
-        await get_safra_data(page, token)
-        await get_data_inter(page, token)
-        await get_data_btg(page, token)
-
-        await page.close()
-        await browser.close()
-
-        return {dataProcessed: true}
-      }, queueConfig
-    )
-
-    StockWorker.on('completed',
-      (job) => {
-        console.log('atualizado com sucesso')
-      }
-    )
-
-    StockWorker.on('failed',
-      (job, error) => {
-        console.log('Algo deu errado')
-        console.error(error)
-      }
-    )
-
+    StockQueue.add('stock', stock)
   }
 }
+
+
+const StockWorker = new Worker('stock',
+  async (job) => {
+    const {data: token} = job
+
+    console.log(token)
+
+    const browser = await puppeteer.launch(puppeteerConfig)
+    const page = await browser.newPage()
+    
+    await get_data_xp(token)
+    await get_safra_data(page, token)
+    await get_data_inter(page, token)
+    await get_data_btg(page, token)
+
+    await page.close()
+    await browser.close()
+
+    return {dataProcessed: true}
+  }, queueConfig
+)
+
+StockWorker.on('completed',
+  (job) => {
+    console.log(job.data, 'atualizado com sucesso')
+  }
+)
+
+StockWorker.on('failed',
+  (job, error) => {
+    console.log('Algo deu errado')
+    console.error(error)
+  }
+)
